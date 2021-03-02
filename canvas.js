a4c13bb9c8831943d7a2aaf7aa7274d71ca7d5bb
ui-d <dawiddeveloper@gmail.com>
2021-03-02 22:48:35 +0100

    arc drawing

--- canvas.js
+++ canvas.js
@@ -12,8 +12,8 @@ let c = canvas.getContext("2d");
 
 function drawCollage(num) {
   for (let i = 0; i < num; i++) {
-    let x = Math.floor(Math.random() * window.innerWidth);
-    let y = Math.floor(Math.random() * window.innerHeight);
+    let x = Math.random() * window.innerWidth;
+    let y = Math.random() * window.innerHeight;
 
     c.fillRect(x, y, 100, 100);
   }
@@ -29,5 +29,13 @@ c.lineTo(120, 400);
 c.lineTo(220, 120);
 c.lineTo(30, 100);
 c.lineTo(140, 220);
+c.lineWidth = 33;
 c.strokeStyle = "rgba(100, 100, 0)";
+c.stroke();
+
+//----------------------------------------------------------------
+
+c.beginPath();
+c.arc(440, 140, 100, 0, 2 * Math.PI, true);
+c.strokeStyle = "limegreen";
 c.stroke();
