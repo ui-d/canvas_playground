d2e72c1feb5839e107d276d3cb8ebec826f41fb7
ui-d <dawiddeveloper@gmail.com>
2021-03-03 23:02:18 +0100

    scaffolding requestAnimationFrame file

---
+++ requestAnimationFrame.js
@@ -0,0 +1,19 @@
+let canvas = document.querySelector("#canvas");
+
+console.log("test");
+
+canvas.width = window.innerWidth - 40;
+canvas.height = 500;
+
+let c = canvas.getContext("2d");
+
+c.beginPath();
+c.moveTo(100, 20);
+c.lineTo(200, 160);
+c.quadraticCurveTo(230, 200, 250, 120);
+c.bezierCurveTo(290, -40, 300, 200, 400, 150);
+c.lineTo(500, 90);
+
+c.lineWidth = 5;
+c.strokeStyle = "white";
+c.stroke();
